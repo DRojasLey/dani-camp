a2d12988b61a0e06b2390e9ac2ab933d7621ab59
dfrojasley <dfrojas.feld@gmail.com>
2024-07-31 21:02:03 -0500

    Reindented Lines

--- projects/project1/cyperExample2.js
+++ projects/project1/cyperExample2.js
@@ -6,14 +6,14 @@
 function generateKey(str,key)
 {
     
-     key=key.split("");
+    key=key.split("");
     if(str.length == key.length)
         return key.join("");
     else
     {
         let temp=key.length;    
         for (let i = 0;i<(str.length-temp) ; i++)
-        {
+            {
             
             key.push(key[i % ((key).length)])
         }
@@ -26,15 +26,15 @@ function generateKey(str,key)
 function cipherText(str,key)
 {
     let cipher_text="";
- 
+    
     for (let i = 0; i < str.length; i++)
-    {
+        {
         // converting in range 0-25
         let x = (str[i].charCodeAt(0) + key[i].charCodeAt(0)) %26;
- 
+        
         // convert into alphabets(ASCII)
         x += 'A'.charCodeAt(0);
- 
+        
         cipher_text+=String.fromCharCode(x);
     }
     return cipher_text;
@@ -45,13 +45,13 @@ function cipherText(str,key)
 function originalText(cipher_text,key)
 {
     let orig_text="";
- 
+    
     for (let i = 0 ; i < cipher_text.length ; i++)
-    {
+        {
         // converting in range 0-25
         let x = (cipher_text[i].charCodeAt(0) -
-                    key[i].charCodeAt(0) + 26) %26;
- 
+        key[i].charCodeAt(0) + 26) %26;
+        
         // convert into alphabets(ASCII)
         x += 'A'.charCodeAt(0);
         orig_text+=String.fromCharCode(x);
@@ -65,9 +65,9 @@ function LowerToUpper(s)
 {
     let str =(s).split("");
     for(let i = 0; i < s.length; i++)
-    {
-        if(s[i] == s[i].toLowerCase())
         {
+        if(s[i] == s[i].toLowerCase())
+            {
             str[i] = s[i].toUpperCase();
         }
     }
@@ -88,7 +88,7 @@ let key = generateKey(str, keyword);
 let cipher_text = cipherText(str, key);
 
 console.log("Ciphertext : "
-                   + cipher_text + "<br><br>");
-
-console.log("Original/Decrypted Text : "
-                   + originalText(cipher_text, key)+"<br>");
+    + cipher_text + "<br><br>");
+    
+    console.log("Original/Decrypted Text : "
+        + originalText(cipher_text, key)+"<br>");
